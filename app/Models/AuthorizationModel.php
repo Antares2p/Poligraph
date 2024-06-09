bbc1cbf959c1a63dce9c98df1a0dba6a7461d706
Antares2p <vetrenko.d@mail.ru>
2024-06-09 14:28:07 +0300

    1

---
+++ app/Models/AuthorizationModel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AuthorizationModel extends Model
+{
+    use HasFactory;
+}
